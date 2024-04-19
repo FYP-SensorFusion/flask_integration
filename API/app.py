2d87f8650e330fb103f9e5788d4010d6e6e8591e
Sahan Caldera <85353320+SahanDC@users.noreply.github.com>
2024-04-19 17:17:05 +0530

    Update app.py

--- API/app.py
+++ API/app.py
@@ -2,8 +2,33 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from text_cleaner import text_cleaner
 from vectorize import vectorize
-import joblib
+import requests
 
+'''
+Hugging face API
+'''
+API_URL = "https://api-inference.huggingface.co/models/ShreyaR/finetuned-roberta-depression"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+def json_to_text(data):
+	for item in data:
+		highest = item[0]
+		highest_label = highest['label']
+		if highest_label == 'LABEL_1':
+			highest_label = 'Depression'
+		elif highest_label == 'LABEL_0':
+			highest_label = 'Not Depression'
+		else:
+			highest_label = 'Unknown'
+
+		highest_score = highest['score']
+		
+		return f"Most likely: {highest_label}"
+     
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
@@ -23,16 +48,20 @@ db.create_all()
 def index():
     if request.method == 'POST':
         try:
-            print(request.form['query'])
+            # print(request.form['query'])
             content = request.form['query']
-            content = text_cleaner(content)
-            output = vectorize(content)
-            new_text = Entry(text=content, output=output)
+            output = query({"inputs": content})
+            output_text = json_to_text(output)
+            print(output_text)
+            # print(output_text)
+            # content = text_cleaner(content)
+            # output = vectorize(content)
+            new_text = Entry(text=content, output=output_text)
 
             db.session.add(new_text)
             db.session.commit()
 
-            return output
+            return output_text
         except Exception as e:
             print("Error: ", e)
             return 'There was an error while processing the content.'
